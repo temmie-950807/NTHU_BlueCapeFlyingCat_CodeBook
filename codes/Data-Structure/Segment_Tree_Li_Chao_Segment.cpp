727b819125dfb3d731194ef82703049b46cbede9
niter-s060405 <niter060405@gmail.com>
2026-07-12 18:03:19 +0800

    Optimized Segment_Tree_Li_Chao_Segment.cpp

--- codes/Data-Structure/Segment_Tree_Li_Chao_Segment.cpp
+++ codes/Data-Structure/Segment_Tree_Li_Chao_Segment.cpp
@@ -1,71 +1,63 @@
+/*
+Tested: https://cses.fi/problemset/hack/3227/entry/17905886/
+*/
 // 全部都是 0-based
-// 宣告：LC_Segment_Tree st(n);
+// 宣告：LC_Segment_Tree st(n);  // n = 值域大小，x 的範圍為 [0, n)
 // 函式：
-// update_segment({a, b}, ql, qr)：在 [ql, qr) 插入一條 y=ax+b 的線段
-// query(x)：查詢所有直線在位置 x 的最小值
-const int MAX_V = 1e6+10; // 值域最大值
+// st.update_segment({a, b}, ql, qr)：在 [ql, qr) 插入一條 y=ax+b 的線段
+// st.query(x)：查詢所有線段在位置 x 的最小值
 
-struct LC_Segment_Tree{
-    struct Node{ // y = ax+b
-        int a = 0;
-        int b = INF;
- 
-        int y(int x){
-            return a*x+b;
-        }
+struct LC_Segment_Tree {
+    struct Line { // y = ax + b
+        int a = 0, b = INF;
+        int y(int x) const { return a * x + b; }
     };
-    vector<Node> arr;
- 
-    LC_Segment_Tree(int n = 0){
-        arr.resize(4*n);
+
+    int N;
+    vector<Line> arr;
+    LC_Segment_Tree(int n) : N(n), arr(4 * n + 5) {}
+
+    void insert_line(Line val, int idx, int l, int r) {
+        if (arr[idx].a > val.a) swap(arr[idx], val);
+        if (arr[idx].a == val.a) {
+            arr[idx].b = min(arr[idx].b, val.b);
+            return;
+        }
+        if (val.y(l) >= arr[idx].y(l)) return;
+        if (val.y(r - 1) < arr[idx].y(r - 1)) {
+            arr[idx] = val;
+            return;
+        }
+        int mid = (l + r) / 2;
+        if (val.y(mid) < arr[idx].y(mid)) {
+            swap(arr[idx], val);
+            insert_line(val, 2 * idx + 2, mid, r);
+        }
+        else insert_line(val, 2 * idx + 1, l, mid);
     }
 
-    void update(Node val, int idx = 0, int ll = 0, int rr = MAX_V){
-        if (rr-ll==0) return;
-        if (rr-ll<=1){
-            if (val.y(ll)<arr[idx].y(ll)){
-                arr[idx] = val;
-            }
+    void dfs(Line v, int ql, int qr, int idx, int l, int r) {
+        if (l >= r || r <= ql || l >= qr) return;
+        if (ql <= l && r <= qr) {
+            insert_line(v, idx, l, r);
             return;
         }
- 
-        int mid = (ll+rr)/2;
-        if (arr[idx].a > val.a) swap(arr[idx], val); // 原本的線斜率要比較小
-        if (arr[idx].y(mid) < val.y(mid)){ // 交點在左邊
-            update(val, idx*2+1, ll, mid);
-        }else{ // 交點在右邊
-            swap(arr[idx], val); // 在左子樹中，新線比舊線還要好
-            update(val, idx*2+2, mid, rr);
-        }
-        return;
+        int mid = (l + r) / 2;
+        dfs(v, ql, qr, 2 * idx + 1, l, mid);
+        dfs(v, ql, qr, 2 * idx + 2, mid, r);
     }
-    
-    // 在 [ql, qr) 加上一條 val 的線段
-    void update_segment(Node val, int ql, int qr, int idx = 0, int ll = 0, int rr = MAX_V){
-        if (rr-ll==0) return;
-        if (rr<=ql || qr<=ll) return;
-        if (ql<=ll && rr<=qr){
-            update(val, idx, ll, rr);
-            return;
-        }
- 
-        int mid = (ll+rr)/2;
-        update_segment(val, ql, qr, idx*2+1, ll, mid);
-        update_segment(val, ql, qr, idx*2+2, mid, rr);
-        return;
+
+    int q(int x, int idx, int l, int r) {
+        if (l >= r) return INF;
+        int res = arr[idx].y(x);
+        if (l + 1 == r) return res;
+        int mid = (l + r) / 2;
+        if (x < mid) return min(res, q(x, 2*idx+1, l, mid));
+        else return min(res, q(x, 2*idx+2, mid, r));
     }
- 
-    int query(int x, int idx = 0, int ll = 0, int rr = MAX_V){
-        if (rr-ll==0) return INF;
-        if (rr-ll==1){
-            return arr[idx].y(ll);
-        }
- 
-        int mid = (ll+rr)/2;
-        if (x<mid){
-            return min(arr[idx].y(x), query(x, idx*2+1, ll, mid));
-        }else{
-            return min(arr[idx].y(x), query(x, idx*2+2, mid, rr));
-        }
+
+    void update_segment(Line val, int ql, int qr) {
+        dfs(val, ql, qr, 0, 0, N);
     }
+    int query(int x) { return q(x, 0, 0, N); }
 };
