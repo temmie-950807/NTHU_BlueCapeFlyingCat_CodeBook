c6b3831d8549c38c481bd82d1bad0c387b808838
niter-s060405 <niter060405@gmail.com>
2026-07-21 12:15:59 +0800

    Made Manacher.cpp more useful

--- codes/String/Manacher.cpp
+++ codes/String/Manacher.cpp
@@ -1,18 +1,27 @@
-string Manacher(string str) {
+/*
+Tested : https://cses.fi/paste/92fb6be2b651cc87112ee10/
+         https://cses.fi/paste/cc510df5d0b84d99112ee2e/
+*/
+// 回傳所有最長（無法往兩側擴展）迴文的區間 [l, r], 0-based
+vector<array<int, 2>> Manacher(string str) {
     string tmp = "$#";
     for(char i : str) {
         tmp += i;
         tmp += '#';
     }
 
-    vector<int> p(tmp.size(), 0);
-    int mx = 0, id = 0, len = 0, center = 0;
-    for(int i=1 ; i<(int)tmp.size() ; i++) {
+    int m = tmp.size(), mx = 0, id = 0;
+    vector<int> p(m);
+    vector<array<int, 2>> res;
+    for(int i=1 ; i<m ; i++) {
         p[i] = mx > i ? min(p[id*2-i], mx-i) : 1;
-
         while(tmp[i+p[i]] == tmp[i-p[i]]) p[i]++;
-        if(mx<i+p[i]) mx = i+p[i], id = i;
-        if(len<p[i]) len = p[i], center = i;
+        if (mx < i + p[i]) mx = i + p[i], id = i;
+
+        if (p[i] > 1) {
+            int r = (i / 2) + (p[i] / 2) - 2 + (i & 1);
+            res.push_back({r - p[i] + 2, r});
+        }
     }
-    return str.substr((center-len)/2, len-1);
+    return res;
 }
